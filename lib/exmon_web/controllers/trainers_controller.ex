47a629f22d16abf15710f5423d0cb3e3805158b3
Leonardo Flores <contato@leonardocouy.com>
2022-04-18 13:03:52 -0300

    Add fallback controller to handle errors to client

--- lib/exmon_web/controllers/trainers_controller.ex
+++ lib/exmon_web/controllers/trainers_controller.ex
@@ -1,6 +1,8 @@
 defmodule ExmonWeb.TrainersController do
   use ExmonWeb, :controller
 
+  action_fallback ExmonWeb.FallbackController
+
   def create(conn, params) do
     params
     |> Exmon.create_trainer()
@@ -12,4 +14,6 @@ defmodule ExmonWeb.TrainersController do
     |> put_status(:created)
     |> render("create.json", trainer: trainer)
   end
+
+  defp handle_response({:error, _changeset} = error, _conn), do: error
 end
