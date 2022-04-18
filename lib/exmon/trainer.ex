45e83722195721d059d34585279d73c04328a5a0
Leonardo Flores <contato@leonardocouy.com>
2022-04-18 11:39:18 -0300

    Add validations to Trainer schema

--- lib/exmon/trainer.ex
+++ lib/exmon/trainer.ex
@@ -7,13 +7,24 @@ defmodule Exmon.Trainer do
   schema "trainers" do
     field :name, :string
     field :password_hash, :string
+    field :email, :string
     timestamps()
   end
 
-  @required_params [:name, :password_hash]
+  @required_params [:name, :password_hash, :email]
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
+    |> update_change(:email, &sanitize_email/1)
     |> validate_required(@required_params)
+    |> validate_length(:password_hash, min: 6)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:email)
+  end
+
+  defp sanitize_email(str) do
+    str
+      |> String.trim()
+      |> String.downcase()
   end
 end
